7c45b2b3fe14eb55e0176776e0ff6001e7e76538
Joshua <josshuagarciia000@gmail.com>
2019-08-11 11:37:42 -0700

    Administration Dashboard

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container pt-4">
     <h3>Welcome, {{authUser}}</h3>
+    <h4 class="pt-2 pb-2">Word Count: {{wordCount}}</h4>
 
     <form class="pt-2">
       <div class="form-group">
@@ -8,6 +9,43 @@
         <input type="email" class="form-control" placeholder="Search">
       </div>
     </form>
+
+  
+    <div class="table-responsive">
+      <table class="table table-hover text-center">
+
+        <thead>
+          <tr>
+            <th>Word</th>
+            <th>Translation</th>
+            <th>Status</th>
+            <th>Writter</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="word in words" :key="word.id">
+            <td>{{word.word}}</td>
+            <td>{{word.wordTranslation}}</td>
+            <td>{{word.visible ? 'Public' : 'Needs Revision'}}</td>
+            <td>@{{word.writter.name}}</td>
+            <td>
+              <button type="button" class="btn btn-warning mr-1">
+                <i class="fas fa-pen"></i>
+              </button>
+            </td>
+            <td>
+              <button type="button" class="btn btn-danger ml-1">
+                <i class="fas fa-trash"></i>
+              </button>  
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
   </div>
 </template>
 
@@ -18,13 +56,18 @@ import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'Dashboard',
-  methods: {
-    ...mapActions(['fetchWords'])
-  },
-  computed: mapGetters(['authUser']),
+  methods: mapActions(['fetchWords']),
+  computed: mapGetters(['authUser', 'wordCount', 'words']),
   created() {
     this.fetchWords();
   }
 }
 
 </script>
+
+
+<style scoped>
+  i {
+    color: white;
+  }
+</style>
--- src/store/modules/words.js
+++ src/store/modules/words.js
@@ -2,23 +2,21 @@ import axios from 'axios';
 import {url} from '../../config/config';
 
 const state = {
-    words: []
+    words: [],
+    wordCount: null
 };
 
 const getters = {
     words: state => state.words,
+    wordCount: state => state.wordCount
 };
 
 const mutations = {
     setWords: (state, words) => state.words = words,
+    setWordCount: (state, count) => state.wordCount = count,
 };
 
 const actions = {
-    // Delete a word.
-    deleteWord(state, id) {
-        // TODO: make http call to delete word.
-    },
-
     // Load all the words.
     fetchWords({commit}) {
         return new Promise((resolve, reject) => {
@@ -26,14 +24,11 @@ const actions = {
             .get(`${url}/v2/word`)
             .then(response => {
 
-                console.log(response);
+                // Set words.
+                commit('setWords', response.data.response.document);
 
-                // // Set state.
-                // commit('setAuthData',
-                //     authToken,
-                //     authPermission,
-                //     authUser
-                // );
+                // Set word count
+                commit('setWordCount', response.data.response.count);
 
                 // Resolve Promise.
                 resolve(response);
