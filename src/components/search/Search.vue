adc2e7b22b6313062c93f3b898c11fdcc954fdfa
Joshua <josshuagarciia000@gmail.com>
2019-08-28 16:48:28 -0700

    Added Search component

---
+++ src/components/search/Search.vue
@@ -0,0 +1,14 @@
+<template>
+    <form class="pt-2">
+      <div class="form-group">
+        <label for="search">Search Antyhing.</label>
+        <input type="email" class="form-control" placeholder="Search">
+      </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'Search'
+}
+</script>
