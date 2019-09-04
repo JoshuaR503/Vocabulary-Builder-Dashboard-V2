1e4fad3ad07a58a19fb044234023462fd52c5e2d
Joshua García <josshuagarciia000@gmail.com>
2019-09-03 19:08:05 -0700

    Added Sentry

--- src/main.js
+++ src/main.js
@@ -6,8 +6,21 @@ import store from './store/index';
 // http helper.
 import setup from './lib/helpers/httpInterceptor';
 
+// Sentry
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+
 // init helper.
 setup();
+
+// Sentry Init
+Sentry.init({
+  dsn: 'https://ebd10d57317149b3a79a596021840683@sentry.io/1550804',
+  integrations: [new Integrations.Vue({
+    Vue, 
+    attachProps: true}
+  )],
+});
 
 Vue.config.productionTip = false;
 
